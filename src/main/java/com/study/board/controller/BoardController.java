537e2b3b809a9ae27825c0b92f539bb8939015da
koyoisneverdie <lunacysky92@gmail.com>
2023-07-30 15:27:03 +0900

    글 수정, 삭제, 알림창 띄우기

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,29 +16,64 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    //작성
     @GetMapping("/board/write")
     public String boardWriteForm() {
-
         return "boardwrite";
     }
 
     @PostMapping("/board/writepro")
-    public String boardwritePro(Board board) {
-
+    public String boardwritePro(Board board, Model model) {
         boardService.write(board);
-        return "";
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
     }
 
+    // 목록 조회
     @GetMapping("/board/list")
     public String boardList(Model model) {
-
         model.addAttribute("list", boardService.boardList());
         return "boardlist";
     }
 
+    // 상세 보기
     @GetMapping("/board/view") // localhost:8090/board/view?id=1
     public String boardView(Model model, Integer id) {
         model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
+
+    // 삭제
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id, Model model) {
+        boardService.boardDelete(id);
+        model.addAttribute("message", "삭제가 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+        return "message";
+    }
+
+    // 수정
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model) {
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp);
+        model.addAttribute("message", "게시글 수정이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
+
+        return "message";
+    }
+
+
+
+
 }
